89b1e2f58d56e3f86df4bd1e3c64022e6a2d9b17
Dane Harrigan <dane.harrigan@gmail.com>
2011-01-27 14:55:13 -0500

    changed method name and added rdoc

--- lib/liquify.rb
+++ lib/liquify.rb
@@ -13,23 +13,58 @@ module Liquify
   @@drops = {}
 
   class << self
+    # setup - This method allows you to register tags, drops and filters.
+    # to Liquify in one place.
+    #
+    #   Liquify.setup do |config|
+    #     ...
+    #   end
     def setup
       yield self
     end
 
+    # register_tag - This method is used to make tags available to your
+    # Liquid templates. It accepts a symbol for the tag name and the tag class.
+    #
+    #   Liquify.setup do |config|
+    #     config.register_tag :tag_name, NameTag
+    #   end
     def register_tag(name, klass)
       @@tags[name] = klass
     end
 
+    # register_filters - This method is used to make filters available to
+    # your Liquid templates. It accepts a module of methods. Each method
+    # becomes a Liquid filter.
+    #
+    #   Liquify.setup do |config|
+    #     config.register_filters CustomFilters
+    #   end
     def register_filters(mod)
       @@filters << mod
     end
 
+    # register_drop - This method is used to make drops available to
+    # your Liquid templates. It accepts a symbol for the drop name and
+    # your drop class or a lambda if processing has to be done at time
+    # time of rendering the Liquid template.
+    #
+    #   Liquify.setup do |config|
+    #     config.register_drop :drop_name, NameDrop
+    #     # with a lamda
+    #     config.register_drop :special_name, lambda { Foo.first }
+    #   end
     def register_drop(name, klass)
       @@drops[name] = klass
     end
 
-    def render(template)
+    # invoke = This method handles to rendering of the Liquid template with
+    # all of the registered drops, tags and filters. It accepts the Liquid
+    # template as a string.
+    #
+    #  template = '{{ drop_name.method }}'
+    #  Liquify.invoke(template)
+    def invoke(template)
       args = {}
       @@drops.each { |name, klass| args[name.to_s] = klass.respond_to?(:call) ? klass.call : klass.new }
       @@filters.each { |filter| Liquid::Template.register_filter(filter) }
