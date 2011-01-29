49c5ff193045a31a6f9102217f7c72f6e869be48
Dane Harrigan <dane.harrigan@gmail.com>
2011-01-29 00:45:31 -0500

    one more rename of render to invoke

--- spec/liquify/drop_spec.rb
+++ spec/liquify/drop_spec.rb
@@ -10,15 +10,15 @@ describe Liquify::Methods do
 
     it 'returns the value of a method on the drop instance' do
       template = '{{ bar.first_name }} {{ bar.last_name }}'
-      Liquify.render(template).should == 'Foo Bar'
+      Liquify.invoke(template).should == 'Foo Bar'
     end
 
     it 'returns the output of a labmda with an argument passed' do
-      Liquify.render('{{ bar.full_name }}').should == 'Foo Bar'
+      Liquify.invoke('{{ bar.full_name }}').should == 'Foo Bar'
     end
 
     it 'returns the output of a labmda without an argument passed' do
-      Liquify.render('{{ bar.age }}').should == '25'
+      Liquify.invoke('{{ bar.age }}').should == '25'
     end
   end
 end
