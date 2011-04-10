f0db912a5d245b4a2985dff4787286d542f8da12
Dane Harrigan <dane.harrigan@gmail.com>
2011-04-10 14:19:31 -0400

    added some example docs to liquify/block.rb

--- lib/liquify/block.rb
+++ lib/liquify/block.rb
@@ -1,26 +1,54 @@
 module Liquify
+  # Creating a block with Liquify is as simple as creating a Ruby
+  # class.
+  #
+  #   class FooBlock < Liquify::Block
+  #     def bar(params)
+  #      ...
+  #     end
+  #
+  #     def baz
+  #      ...
+  #     end
+  #   end 
+  #
+  #   Liquify.setup do |config|
+  #     config.register_tag :foo_block, FooBlock
+  #   end
+  #
+  #   {% foo_block as: 'f' %}
+  #     {{ f.bar 'arg1', key: 'value' }}
+  #     {{ f.baz }}
+  #   {% endfoo_block %}
+  #
+  # If you need wrapper content override the invoke method
+  # and call yield within it.
+  #
   class Block < Liquid::Block
-    def initialize(tag_name, markup, tokens)
+    def initialize(tag_name, markup, tokens) # :nodoc:
       @tokens = tokens.dup
       super(tag_name, markup, tokens)
     end
 
-    def render(context)
-      params = Liquify::Parameter.new(@markup)
-      options = params.extract_options!
+    def render(context) # :nodoc:
+      params = Liquify::Parameter.new(@markup, context)
+      options = params.dup.extract_options!
       drop_tokens = @tokens.grep /\{\{\s*#{options['as']}\..*\}\}/
 
-      context[options['as']] = Liquify::Block::Drop.new(self, drop_tokens)
+      context[options['as']] = Liquify::Block::Drop.new(self, drop_tokens, context)
+
+      args = []
+      args << params if method(:invoke).arity == 1
 
       context.stack do
-        invoke do
+        invoke(*args) do
           render_all(@nodelist, context).join
         end
       end
     end
 
     class Drop < Liquid::Drop
-      def initialize(obj, tokens)
+      def initialize(obj, tokens, context) # :nodoc:
         @obj = obj
         @params = {}
 
@@ -28,11 +56,11 @@ module Liquify
           match, key, value = t.match(/\{\{\s*f\.(\w+)(.*)\}\}/).to_a
 
           @params[key] ||= []
-          @params[key] << Liquify::Parameter.new(value)
+          @params[key] << Liquify::Parameter.new(value, context)
         end
       end
 
-      def invoke_drop(key)
+      def invoke_drop(key) # :nodoc:
         allowed_methods = @obj.public_methods(false) - [:invoke]
         return unless allowed_methods.include?(key.to_sym)
 
@@ -40,8 +68,6 @@ module Liquify
       end
 
       alias :[] :invoke_drop
-
-      private
     end
   end
 end
